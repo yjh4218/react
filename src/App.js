877026c92fa96ad68ab0da42ec5c7a46a85549ef
yjh4218 <84125522+yjh4218@users.noreply.github.com>
2021-09-02 23:49:17 +0900

    6강 완료

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
+import { useEffect, useState } from "react"; // react 라이브러리의 useEffect 사용
 import "./App.css";
 
 function App() {
-  let list = [1, 2, 3];
+  const [data, setData] = useState(0);
+
+  const download = () => {
+    // 다운로드 받고(통신)
+    let downloadData = 5; // 가정
+    setData(downloadData);
+  };
+
+  // 실행시점
+  // 1. App()함수가 최초 실행될 때 실행함. 최초 마운트 될 때(그림이 그려질 때)
+  // 2. 상태변수가 변경될 때
+  useEffect(() => {
+    console.log("useEffect 실행됨");
+    download();
+  }, []);
 
   return (
     <div>
-      <div>
-        {list.map(
-          (n) => (
-            <h1>{n}</h1>
-          ), // tag 사용시 n 에 중괄호 사용. 없으면 중괄호 사용 안 해도 됨.
-        )}
-      </div>
-      ;
+      <h1>데이터 : {data}</h1>
+      <button
+        onClick={() => {
+          setData(data + 1);
+        }}
+      >
+        더하기
+      </button>
     </div>
   );
 }
---
+++ src/Sub.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+let num = 10;
+
+function Sub() {
+  return <div></div>;
+}
+
+export { num }; // 변수도 export 가능함
+export default Sub;
